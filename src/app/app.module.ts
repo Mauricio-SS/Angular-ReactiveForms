4d01f1219aff8e50776d1cca6620f347f2aeb3f9
Mau-SS <mauricosandoval999@gmail.com>
2022-10-05 21:43:29 -0500

    Clase 1 formularios reactivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearComponent } from './crear/crear.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearComponent
   ],
   imports: [
     BrowserModule,
